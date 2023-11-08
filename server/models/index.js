2f3fb2328eb253e4cd49985f55dbdb23efa1e28f
cverges1 <122914738+cverges1@users.noreply.github.com>
2023-11-08 15:45:32 -0700

    added to server, connected to db, created models

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+//here we are imporing all of our models
+const User = require('./User');
+const Category = require('./Category');
+const Product = require('./Product');
+
+module.exports = { User, Category, Product };
