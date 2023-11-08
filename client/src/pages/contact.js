4dbcc227989911aba03e478cad202fb102496d91
cverges1 <122914738+cverges1@users.noreply.github.com>
2023-11-08 13:35:04 -0700

    edited nav bar and added pages

---
+++ client/src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BasicCard from "../components/card";
+
+const Contact = () => {
+  return (
+  <React.Fragment>
+    <h1>TEST Contact</h1>
+    <BasicCard></BasicCard>
+  </React.Fragment>)
+};
+
+export default Contact;
---
+++ client/src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BasicCard from "../components/card";
+
+const Home = () => {
+  return (
+  <React.Fragment>
+    <h1>TEST HOME</h1>
+    <BasicCard></BasicCard>
+  </React.Fragment>)
+};
+
+export default Home;
