a76c1e5cebab81594acdc15fbea5369aef6244fc
cverges1 <122914738+cverges1@users.noreply.github.com>
2023-12-05 15:26:50 -0700

    major styling changes throughout

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -2,38 +2,6 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
-import GlobalStyles from '@mui/material/GlobalStyles';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Container from '@mui/material/Container';
-
-
-const defaultTheme = createTheme();
-
-const footers = [
-  {
-    title: 'Company',
-    description: ['Team', 'History', 'Contact us', 'Locations'],
-  },
-  {
-    title: 'Features',
-    description: [
-      'Cool stuff',
-      'Random feature',
-      'Team feature',
-      'Developer stuff',
-      'Another one',
-    ],
-  },
-  {
-    title: 'Resources',
-    description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
-  },
-  {
-    title: 'Legal',
-    description: ['Privacy policy', 'Terms of use'],
-  },
-];
 
 export default function StickyFooter() {
   return (
--- client/src/components/SingleProd/index.js
+++ client/src/components/SingleProd/index.js
@@ -8,6 +8,8 @@ import Typography from "@mui/material/Typography";
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { QUERY_SINGLE_PROD } from "../../utils/queries";
+import { Box } from "@mui/system";
+import { CardHeader } from "@mui/material";
 
 export default function SingleProduct() {
   const { id } = useParams();
@@ -32,29 +34,77 @@ export default function SingleProduct() {
   console.log(product);
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <Card sx={{ width: '50vw' }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Card
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "75vw",
+          maxHeight: "75vh",
+        }}
+      >
+        <CardHeader
+          title={
+            <Typography variant="h4" align="center">
+              {product.name}
+            </Typography>
+          }
+          titleTypographyProps={{ align: "center" }}
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[200]
+                : theme.palette.grey[700],
+          }}
+        />
         <CardMedia
           component="img"
           alt={`${product.name}`}
           src={`/images/${product.image}`}
+          sx={{ minWidth: "75vw", maxHeight: "50vh" }}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {product.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            ${product.price}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {product.description}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button fullWidth variant="contained">
-            ADD TO CART
-          </Button>
-        </CardActions>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            flex: 1,
+            justifyContent: "space-between",
+          }}
+        >
+          <Box sx={{ flex: "1 0 auto" }}>
+            <CardContent>
+              <Typography
+                variant="subtitle"
+                color="text.secondary"
+                component="div"
+                textAlign={"center"}
+                sx={{ mb: 1 }}
+              >
+                {product.description}
+              </Typography>
+              <Typography
+                variant="subtitle"
+                color="text.secondary"
+                component="div"
+                textAlign={"center"}
+              >
+                ${product.price}
+              </Typography>
+            </CardContent>
+          </Box>
+          <CardActions>
+            <Button fullWidth variant="contained">
+              ADD TO CART
+            </Button>
+          </CardActions>
+        </Box>
       </Card>
     </div>
   );
