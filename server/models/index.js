69f4a495a7ed34ddfa062415b61a676085f97ebc
cverges1 <122914738+cverges1@users.noreply.github.com>
2023-12-09 17:02:09 -0700

    got cart and checkout working but need to resolve 400 error bug after purchase

--- server/models/index.js
+++
@@ -1,8 +0,0 @@
-//here we are imporing all of our models
-const User = require('./User');
-const Category = require('./Category');
-const Product = require('./Product');
-const SubCategory = require('./SubCategory');
-const Order = require('./Order');
-
-module.exports = { User, Category, Product, SubCategory, Order };
